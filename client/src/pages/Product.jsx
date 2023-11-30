da2b8aea3651df60deb8c606b1c2b9afaaa3ff31
Martin <Martin.Andre.Jarneid@ssn.no>
2023-11-30 12:28:55 +0100

    Add loading indicator and 404 redirect

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import LoadingBackdrop from "../components/Loading";
 
 const Product = () => {
     const navigate = useNavigate();
     const { id } = useParams();
     const [product, setProduct] = useState(null);
     const [showDeleteModal, setShowDeleteModal] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         (async () => {
@@ -13,16 +15,20 @@ const Product = () => {
             const fetchedProduct = await res.json();
             if (res.ok) {
                 setProduct(fetchedProduct.data);
+                return;
             }
+            navigate("/not_found");
         })();
-    }, [id]);
+    }, [id, navigate]);
 
     const deleteProduct = async () => {
+        setIsLoading(true);
         try {
             await fetch(`/api/products/${id}`, { method: "DELETE" });
             navigate("/products");
         } catch {
             // set error message
+            setIsLoading(false);
         }
     };
 
@@ -40,79 +46,82 @@ const Product = () => {
     }, [showDeleteModal]);
 
     return (
-        <div className="card card-side bg-base-100 shadow-xl">
-            {product && (
-                <>
-                    <figure>
-                        <img
-                            src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-                            alt="Movie"
-                        />
-                    </figure>
-                    <div className="card-body">
-                        <h2 className="card-title">{product.name}</h2>
-                        <p>{product.description}</p>
-                        <p>Pris: {product.price?.NOK}</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-primary btn-accent">
-                                Edit
-                            </button>
-                            <button
-                                className="btn btn-primary btn-error"
-                                onClick={() => {
-                                    setShowDeleteModal(true);
-                                }}
-                            >
-                                Delete
-                            </button>
-                        </div>
-                    </div>
-                    {showDeleteModal && (
-                        <>
-                            {/* extract this
-                            add spinner when trying to delete */}
-                            <dialog
-                                id="my_modal_5"
-                                className="modal modal-bottom sm:modal-middle modal-open"
-                            >
-                                <div className="modal-box">
-                                    <h3 className="font-bold text-lg">
-                                        Delete
-                                    </h3>
-                                    <p className="py-4">
-                                        Are you sure you want to delete this
-                                        product?
-                                    </p>
-                                    <div className="modal-action">
-                                        <form method="dialog">
-                                            <button
-                                                className="btn btn-error"
-                                                onClick={deleteProduct}
-                                            >
-                                                Delete
-                                            </button>
-                                        </form>
-                                    </div>
-                                </div>
-                                <form
-                                    method="dialog"
-                                    className="modal-backdrop"
+        <>
+            {isLoading && <LoadingBackdrop />}
+            <div className="card card-side bg-base-100 shadow-xl">
+                {product && (
+                    <>
+                        <figure>
+                            <img
+                                src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+                                alt="Movie"
+                            />
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">{product.name}</h2>
+                            <p>{product.description}</p>
+                            <p>Pris: {product.price?.NOK}</p>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary btn-accent">
+                                    Edit
+                                </button>
+                                <button
+                                    className="btn btn-primary btn-error"
+                                    onClick={() => {
+                                        setShowDeleteModal(true);
+                                    }}
                                 >
-                                    <button
-                                        className="cursor-default"
-                                        onClick={() => {
-                                            setShowDeleteModal(false);
-                                        }}
+                                    Delete
+                                </button>
+                            </div>
+                        </div>
+                        {showDeleteModal && (
+                            <>
+                                {/* extract this
+                            add spinner when trying to delete */}
+                                <dialog
+                                    id="my_modal_5"
+                                    className="modal modal-bottom sm:modal-middle modal-open"
+                                >
+                                    <div className="modal-box">
+                                        <h3 className="font-bold text-lg">
+                                            Delete
+                                        </h3>
+                                        <p className="py-4">
+                                            Are you sure you want to delete this
+                                            product?
+                                        </p>
+                                        <div className="modal-action">
+                                            <form method="dialog">
+                                                <button
+                                                    className="btn btn-error"
+                                                    onClick={deleteProduct}
+                                                >
+                                                    Delete
+                                                </button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                    <form
+                                        method="dialog"
+                                        className="modal-backdrop"
                                     >
-                                        close
-                                    </button>
-                                </form>
-                            </dialog>
-                        </>
-                    )}
-                </>
-            )}
-        </div>
+                                        <button
+                                            className="cursor-default"
+                                            onClick={() => {
+                                                setShowDeleteModal(false);
+                                            }}
+                                        >
+                                            close
+                                        </button>
+                                    </form>
+                                </dialog>
+                            </>
+                        )}
+                    </>
+                )}
+            </div>
+        </>
     );
 };
 
