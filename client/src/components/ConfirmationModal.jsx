8b2c77d929e9f450842b0e76176db4a1fd563321
Martin <Martin.Andre.Jarneid@ssn.no>
2023-12-20 14:39:05 +0100

    Refactor

--- client/src/components/ConfirmationModal.jsx
+++ client/src/components/ConfirmationModal.jsx
@@ -21,10 +21,7 @@ const ConfirmationModal = ({
     }, [setShowModal]);
 
     return (
-        <dialog
-            id="my_modal_5"
-            className="modal modal-bottom sm:modal-middle modal-open"
-        >
+        <dialog className="modal modal-bottom sm:modal-middle modal-open">
             <div className="modal-box">
                 <h3 className="font-bold text-lg">{title}</h3>
                 <p className="py-4">{text}</p>
--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -14,13 +14,15 @@ const Product = () => {
 
     useEffect(() => {
         (async () => {
+            setIsLoading(true);
             const res = await fetch(`/api/products/${id}`);
             const fetchedProduct = await res.json();
             if (res.ok) {
                 setProduct(fetchedProduct.data);
-                return;
+            } else {
+                setNotFound(true);
             }
-            setNotFound(true);
+            setIsLoading(false);
         })();
     }, [id]);
 
@@ -30,12 +32,10 @@ const Product = () => {
             await fetch(`/api/products/${id}`, { method: "DELETE" });
             navigate("/products");
         } catch {
-            // set error message
+            // set error message here
             setIsLoading(false);
         }
     };
-
-    
 
     return (
         <>
@@ -43,68 +43,65 @@ const Product = () => {
             {notFound && <ErrorPage />}
             {product && (
                 <div className="card card-side bg-base-100 shadow-xl">
-                    <>
-                        <figure className="self-start">
-                            <img
-                                src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-                                alt="Movie"
-                            />
-                        </figure>
-                        <div className="card-body">
-                            <h2 className="card-title">{product.name}</h2>
-                            <p>{product.description}</p>
-                            <p>Pris: {product.price?.NOK}</p>
-                            {product.categories && (
-                                <>
-                                    <h3 className="text-lg mt-8">Categories</h3>
-                                    <ul>
-                                        {product.categories.map((cat) => {
-                                            return (
-                                                <li key={cat._id}>
-                                                    <Link
-                                                        className="link"
-                                                        to={
-                                                            "/categories/" +
-                                                            cat._id
-                                                        }
-                                                    >
-                                                        {cat.name}
-                                                    </Link>
-                                                </li>
-                                            );
-                                        })}
-                                    </ul>
-                                </>
-                            )}
-                            <div className="card-actions justify-end">
-                                <button
-                                    className="btn btn-primary btn-accent"
-                                    onClick={() => {
-                                        navigate("edit");
-                                    }}
-                                >
-                                    Edit
-                                </button>
-                                <button
-                                    className="btn btn-primary btn-error"
-                                    onClick={() => {
-                                        setShowDeleteModal(true);
-                                    }}
-                                >
-                                    Delete
-                                </button>
-                            </div>
-                        </div>
-                        {showDeleteModal && (
-                            <ConfirmationModal
-                                title="Delete"
-                                text="Are you sure you want to delete this product?"
-                                buttonText="Delete"
-                                onClick={deleteProduct}
-                                setShowModal={setShowDeleteModal}
-                            />
+                    <figure className="self-start">
+                        <img
+                            src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+                            alt="Movie"
+                        />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">{product.name}</h2>
+                        <p>{product.description}</p>
+                        <p>Pris: {product.price?.NOK}</p>
+                        {product.categories && (
+                            <>
+                                <h3 className="text-lg mt-8">Categories</h3>
+                                <ul>
+                                    {product.categories.map((cat) => {
+                                        return (
+                                            <li key={cat._id}>
+                                                <Link
+                                                    className="link"
+                                                    to={
+                                                        "/categories/" + cat._id
+                                                    }
+                                                >
+                                                    {cat.name}
+                                                </Link>
+                                            </li>
+                                        );
+                                    })}
+                                </ul>
+                            </>
                         )}
-                    </>
+                        <div className="card-actions justify-end">
+                            <button
+                                className="btn btn-primary btn-accent"
+                                onClick={() => {
+                                    navigate("edit");
+                                }}
+                            >
+                                Edit
+                            </button>
+                            <button
+                                className="btn btn-primary btn-error"
+                                onClick={() => {
+                                    setShowDeleteModal(true);
+                                }}
+                            >
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+                    {showDeleteModal && (
+                        <ConfirmationModal
+                            title="Delete"
+                            text="Are you sure you want to delete this product?"
+                            buttonText="Delete"
+                            onClick={deleteProduct}
+                            setShowModal={setShowDeleteModal}
+                        />
+                    )}
                 </div>
             )}
         </>
