4afb07424c8837efe995194e7bf35e3064479bfb
Martin <Martin.Andre.Jarneid@ssn.no>
2023-12-20 16:35:32 +0100

    Add scrolling for table on small screens

--- client/src/pages/ProductList.jsx
+++ client/src/pages/ProductList.jsx
@@ -25,53 +25,55 @@ const ProductList = () => {
                     New
                 </Link>
             </div>
-            <table className="table table-zebra">
-                <thead>
-                    <tr className="text-sm">
-                        <th>Name</th>
-                        <th>Price (kr)</th>
-                        <th>Categories</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {products &&
-                        products.map((product) => {
-                            return (
-                                <tr key={product._id} className="hover">
-                                    <td
-                                        className="cursor-pointer"
-                                        onClick={() => {
-                                            navigate(product._id);
-                                        }}
-                                    >
-                                        {product.name}{" "}
-                                    </td>
-                                    <td
-                                        className="cursor-pointer"
-                                        onClick={() => {
-                                            navigate(product._id);
-                                        }}
-                                    >
-                                        {formatNumber(product.price.NOK)}{" "}
-                                    </td>
-                                    <td className="truncate max-w-xs">
-                                        {product.categories.map((cat) => {
-                                            return (
-                                                <Link
-                                                    key={cat._id}
-                                                    to={`/categories/${cat._id}`}
-                                                >
-                                                    {" "}
-                                                    {cat.name}
-                                                </Link>
-                                            );
-                                        })}
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                </tbody>
-            </table>
+            <div className="overflow-x-scroll">
+                <table className="table table-zebra">
+                    <thead>
+                        <tr className="text-sm">
+                            <th>Name</th>
+                            <th>Price (kr)</th>
+                            <th>Categories</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {products &&
+                            products.map((product) => {
+                                return (
+                                    <tr key={product._id} className="hover">
+                                        <td
+                                            className="cursor-pointer"
+                                            onClick={() => {
+                                                navigate(product._id);
+                                            }}
+                                        >
+                                            {product.name}{" "}
+                                        </td>
+                                        <td
+                                            className="cursor-pointer"
+                                            onClick={() => {
+                                                navigate(product._id);
+                                            }}
+                                        >
+                                            {formatNumber(product.price.NOK)}{" "}
+                                        </td>
+                                        <td className="truncate max-w-xs">
+                                            {product.categories.map((cat) => {
+                                                return (
+                                                    <Link
+                                                        key={cat._id}
+                                                        to={`/categories/${cat._id}`}
+                                                    >
+                                                        {" "}
+                                                        {cat.name}
+                                                    </Link>
+                                                );
+                                            })}
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                    </tbody>
+                </table>
+            </div>
         </>
     );
 };
